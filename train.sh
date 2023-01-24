47636f184635d19e3850c953ffe4fbe932065fe6
Vimal M <61645553+vimal-m@users.noreply.github.com>
2023-01-24 12:18:19 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,7 +4,10 @@ echo "converting conll datafile to spacy DocBin..."
 
 python3 -m spacy convert data/trainfile.conll data/ -c conll
 
-echo "initializing config and begin training..."
-python3 -m spacy init config - --gpu --lang en --pipeline ner --optimize accuracy | python3 -m spacy train - --output models/ --paths.train ./data/trainfile.spacy --paths.dev ./data/trainfile.spacy --gpu-id 0
+echo "initializing config..."
+#python3 -m spacy init config - --gpu --lang hi --pipeline ner --optimize accuracy | python3 -m spacy train - --output models/ --paths.train ./data/trainfile.spacy --paths.dev ./data/trainfile.spacy --gpu-id 0
+python3 -m spacy init fill-config base_config.cfg config.cfg
+echo "begin training..."
+python3 -m spacy train config.cfg --output models/ --paths.train ./data/trainfile.spacy --paths.dev ./data/trainfile.spacy --gpu-id 0
 
 echo "training done..."
